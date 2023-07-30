8e44d78def756ba78af15bf07b33d9b4cb4808d8
Muhammad Alful Falakh <alfulfal@gmail.com>
2023-07-30 15:33:46 +0700

    Login + register ui

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,26 +1,35 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
     <div class="container">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link {{($active==='home') ? 'active' : ''}}" href="/">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link {{($active==='about') ? 'active' : ''}}" href="/about">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link {{($active==='post') ? 'active' : ''}}" href="/posts">Blog</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link {{($active==='categories') ? 'active' : ''}}" href="/categories">Categories</a>
-          </li>
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link {{ $active === 'home' ? 'active' : '' }}" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ $active === 'about' ? 'active' : '' }}" href="/about">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ $active === 'post' ? 'active' : '' }}" href="/posts">Blog</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ $active === 'categories' ? 'active' : '' }}" href="/categories">Categories</a>
+                </li>
 
-        </ul>
-      </div>
+            </ul>
+
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-tem {{ $active === 'login' ? 'active' : '' }}">
+                    <a href="/login" class="nav-link">
+                        <i class="bi bi-box-arrow-in-right"></i>
+                        Login</a>
+
+                </li>
+            </ul>
+        </div>
     </div>
-  </nav>
-  
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
-use App\Models\Post;
-use App\Models\User;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -114,3 +114,6 @@ Route::get('/categories', function () {
 //         'posts' => $author->posts->load('category', 'author')
 //     ]);
 // });
+
+Route::get('/login', [LoginController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'index']);
