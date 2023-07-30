f1170ad4419ce89e243ad138d793fcd4a3d46765
Muhammad Alful Falakh <alfulfal@gmail.com>
2023-07-30 22:47:39 +0700

    Register

--- routes/web.php
+++ routes/web.php
@@ -117,3 +117,5 @@ Route::get('/categories', function () {
 
 Route::get('/login', [LoginController::class, 'index']);
 Route::get('/register', [RegisterController::class, 'index']);
+//nyimpan data
+Route::post('/register', [RegisterController::class, 'store']);
